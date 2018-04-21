55caeb3ce7d65ee3e591e56c51392fb2379a6b0c
carlospolop <carlospolop@gmail.com>
2018-04-21 13:23:50 +0200

    WOT-V1

---
+++ README.md
@@ -0,0 +1,43 @@
+# Node Simple Web Of Trust Api
+
+CUse Node to check for malicious domains in [WOT](https://www.mywot.com/)
+
+## Install
+`npm install weboftrust`
+
+## How to use
+
+```javascript
+const weboftrust = require('weboftrust');
+
+weboftrust.setApiKey = "<YOUR API KEY>"
+
+// Use checkDomains([<domain>,<domain>...]) to check that domains against WOT
+// A promise will be returned
+var domains = ["example.com", "www.google.com"]
+weboftrust.checkDomains(domains).then(function(result){ 
+    console.log(result);
+}, function(err) {
+    console.log(err);
+});
+
+/*
+Output example
+{ 'example.com':
+   { '0': [ 94, 55 ],
+     '1': [ 94, 55 ],
+     '2': [ 94, 55 ],
+     '4': [ 94, 54 ],
+     target: 'example.com',
+     categories: { '304': 47, '501': 95 } },
+  'www.google.com':
+   { '0': [ 94, 69 ],
+     '1': [ 94, 69 ],
+     '2': [ 94, 69 ],
+     '4': [ 93, 66 ],
+     target: 'google.com',
+     categories: { '301': 47, '304': 6, '501': 99 } 
+    } 
+}
+*/
+```
---
+++ weboftrust.js
@@ -0,0 +1,35 @@
+const request = require('http');
+
+var apiKey = "";
+
+function checkDomains(hosts){
+    var promise = new Promise(function(resolve, reject) {
+        if (apiKey == "" ) reject(Error("Please set the user and password"));
+
+        var str = ''
+        var path = '/0.4/public_link_json2?hosts=';
+        hosts.forEach(function(host){
+            path += host + '/'
+        })
+        path += '&key='+apiKey;
+        var options = {
+            host: 'api.mywot.com',
+            path: path
+        };
+        
+          http.request(options, function(res){
+              res.on('data', function(chunk){
+                str += chunk;
+              });
+              res.on('end', function () {
+                result = JSON.parse(str);
+                if (result == '' || result == {}) reject(Error("Error using Web Of Trust: "+error));
+                resolve(result);
+              });
+          }).end();
+    });
+    return promise;
+}
+
+exports.setApiKey = function(key){ apiKey = key; }
+exports.checkDomains = checkDomains;
